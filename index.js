c021beb6bc815a4f21bcd7d9c2f923a74d79e46f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-12 13:32:42 +0800

    Refactor

--- index.js
+++ index.js
@@ -3,49 +3,50 @@
 var tagExpr = /^<!--\{(.*)\}-->$/
 
 module.exports = function attributes (md) {
-  function curlyAttrs (state) {
-    var tokens = state.tokens
-    var crumbs = []
-    var lastParent
-    var m
-
-    var omissions = []
-
-    tokens.forEach(function (token, i) {
-      // Save breadcrumbs so html_block will pick it up
-      if (token.type.match(/_(open|start)$/)) crumbs.push(token)
-      else if (token.type.match(/_(close|end)$/)) lastParent = crumbs.pop()
-
-      if (token.type === 'html_block') {
-        m = token.content.match(tagExpr)
-        if (!m) return
-
-        applyToToken(lastParent, m[1])
-        omissions.unshift(i)
-      }
-
-      // { type: 'inline', children: { ..., '<!--{...}-->' } }
-      if (token.type === 'inline') {
-        var lastChild = token.children[token.children.length - 1]
-        if (!lastChild) return
-
-        m = lastChild.content.match(tagExpr)
-        if (!m) return
-
-        token.children.pop()
-        trimRight(token.children[token.children.length - 1], 'content')
-        applyToToken(tokens[i - 1], m[1])
-      }
-    })
-
-    // Remove <!--...--> html_block tokens
-    omissions.forEach(function (idx) {
-      tokens = tokens.splice(idx, 1)
-    })
-  }
-
   md.core.ruler.push('curly_attributes', curlyAttrs)
 }
+
+function curlyAttrs (state) {
+  var tokens = state.tokens
+  var crumbs = []
+  var omissions = []
+  var lastParent, m
+
+  tokens.forEach(function (token, i) {
+    // Save breadcrumbs so html_block will pick it up
+    if (token.type.match(/_(open|start)$/)) crumbs.push(token)
+    else if (token.type.match(/_(close|end)$/)) lastParent = crumbs.pop()
+
+    // "# Hello\n<!--{.classname}-->"
+    // ...sequence of [heading_open, inline, heading_cose, html_block]
+    if (token.type === 'html_block') {
+      m = token.content.match(tagExpr)
+      if (!m) return
+
+      applyToToken(lastParent, m[1])
+      omissions.unshift(i)
+    }
+
+    // "# Hello <!--{.classname} -->"
+    // { type: 'inline', children: { ..., '<!--{...}-->' } }
+    if (token.type === 'inline') {
+      var lastChild = token.children[token.children.length - 1]
+      m = lastChild && lastChild.content.match(tagExpr)
+      if (!m) return
+
+      // Remove the comment, then remove the extra space
+      token.children.pop()
+      trimRight(token.children[token.children.length - 1], 'content')
+      applyToToken(tokens[i - 1], m[1])
+    }
+  })
+
+  // Remove <!--...--> html_block tokens
+  omissions.forEach(function (idx) {
+    tokens = tokens.splice(idx, 1)
+  })
+}
+
 
 /**
  * Private: trim the right
@@ -57,6 +58,8 @@ function trimRight (obj, attr) {
 
 /**
  * Private: apply tag to token
+ *
+ *     applyToToken(token, '.classname')
  */
 
 function applyToToken (token, attrs) {
@@ -88,9 +91,18 @@ function applyToToken (token, attrs) {
   }
 }
 
+/**
+ * Private: adds a class name to token
+ */
+
 function addClass (token, value) {
   setAttr(token, 'class', value, { append: true })
 }
+
+/**
+ * Private: sets an attribute `attr` to `value` in a token. If `options.append`
+ * is true, append to the old value instead of overwriting it.
+ */
 
 function setAttr (token, attr, value, options) {
   var idx = token.attrIndex(attr)
